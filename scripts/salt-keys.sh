b21aec6526fe8124fc515d7ccfd4df5fac02979c
NereuFajardo <62913299+NereuFajardo@users.noreply.github.com>
2024-05-13 12:16:17 -0300

    salt-keys - update keys instead add.sh

--- scripts/salt-keys.sh
+++ scripts/salt-keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Encontra todos os arquivos wp-config.php no sistema
+# Encontra todos os arquivos wp-config.php
 find / -name wp-config.php 2>/dev/null | while read config_file
 do
     echo "Atualizando as keys no arquivo: $config_file"
@@ -17,16 +17,16 @@ do
         continue  # Pula para o próximo arquivo se falhar em obter as keys
     fi
 
-    # Cria um novo arquivo temporário com as chaves atualizadas
+    # Substitui as chaves antigas pelas novas chaves no arquivo
     awk -v new_keys="$new_keys" '
-        /define\(.*AUTH_KEY/ {print new_keys; next}
-        /define\(.*SECURE_AUTH_KEY/ {next}
-        /define\(.*LOGGED_IN_KEY/ {next}
-        /define\(.*NONCE_KEY/ {next}
-        /define\(.*AUTH_SALT/ {next}
-        /define\(.*SECURE_AUTH_SALT/ {next}
-        /define\(.*LOGGED_IN_SALT/ {next}
-        /define\(.*NONCE_SALT/ {next}
+        /define\(.*AUTH_KEY/ {$0 = "define('AUTH_KEY', '" new_keys "');"}
+        /define\(.*SECURE_AUTH_KEY/ {$0 = "define('SECURE_AUTH_KEY', '" new_keys "');"}
+        /define\(.*LOGGED_IN_KEY/ {$0 = "define('LOGGED_IN_KEY', '" new_keys "');"}
+        /define\(.*NONCE_KEY/ {$0 = "define('NONCE_KEY', '" new_keys "');"}
+        /define\(.*AUTH_SALT/ {$0 = "define('AUTH_SALT', '" new_keys "');"}
+        /define\(.*SECURE_AUTH_SALT/ {$0 = "define('SECURE_AUTH_SALT', '" new_keys "');"}
+        /define\(.*LOGGED_IN_SALT/ {$0 = "define('LOGGED_IN_SALT', '" new_keys "');"}
+        /define\(.*NONCE_SALT/ {$0 = "define('NONCE_SALT', '" new_keys "');"}
         {print}
     ' $config_file > temp_file && mv temp_file $config_file
 
